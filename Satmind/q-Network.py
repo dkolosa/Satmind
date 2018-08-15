d6f856cdfaabbccf84adb4ff52883f6dc1583a71
Daniel Kolosa <contact@dkolosa.com>
2018-08-14 23:53:22 -0400

    Added experience replay class

--- Satmind/q-Network.py
+++ Satmind/q-Network.py
@@ -55,11 +55,28 @@ num_episodes = 1000
 j_list = []
 r_list = []
 
+
+class Experience():
+    def __init__(self, buffer_size):
+        self.buffer_size = buffer_size
+        self.buffer = []
+
+    def experience_replay(self, experience):
+        if len(self.buffer) + len(experience) >= self.buffer_size:
+            self.buffer[0:(len(experience) + len(self.buffer)) - self.buffer_size] = []
+        self.buffer.extend(experience)
+
+
+# experience replay
+experience = Experience(buffer_size=50)
+
 # Network Model
 num_inputs = 16
 num_outputs = 4
 layer_1_nodes = 20
 # layer_2_nodes = 20
+
+
 
 # Establish feed-forward network
 inputs = tf.placeholder(shape=[1,num_inputs], dtype=tf.float32)
@@ -68,8 +85,11 @@ inputs = tf.placeholder(shape=[1,num_inputs], dtype=tf.float32)
 # b1 = tf.variable(tf.zeros[100])
 
 with tf.variable_scope('layer-1'):
-    weights = tf.get_variable(name='weights-1', shape=(num_inputs, layer_1_nodes))
-    layer_1_output = tf.matmul(inputs, weights)
+    weights = tf.get_variable(name='weights-1', shape=(num_inputs, layer_1_nodes),
+                              initializer=tf.contrib.layers.xavier_initializer()
+                              )
+    bias = tf.get_variable(name='bias1', shape=([layer_1_nodes]), initializer=tf.zeros_initializer())
+    layer_1_output = tf.nn.relu(tf.matmul(inputs, weights) + bias)
 
 # with tf.variable_scope('layer-2'):
 #     weights = tf.get_variable(name='weights-2', shape=(layer_1_nodes, layer_2_nodes))
@@ -78,7 +98,9 @@ with tf.variable_scope('layer-1'):
 # weights2 = tf.Variable(tf.random_uniform([16,4]
 with tf.variable_scope('output'):
     weights = tf.get_variable(name='weight-out', shape=(layer_1_nodes, num_outputs))
-    Q_output = tf.matmul(layer_1_output, weights)
+    bias = tf.get_variable(name='bias_out', shape=([num_outputs]), initializer=tf.zeros_initializer())
+
+    Q_output = tf.nn.relu(tf.matmul(layer_1_output, weights) + bias)
 
 predict = tf.argmax(Q_output, 1)
 
@@ -110,6 +132,8 @@ with tf.Session() as sess:
         d = False
         j = 0
 
+        episonde_eperience = Experience(buffer_size=50)
+
         # Q-network
         while j < 200:
             j+=1
@@ -128,6 +152,8 @@ with tf.Session() as sess:
             # The state is the x-y coordinates, r =0 if not reached
             s1, r, d, _ = env.step(a[0])
             # s1 = env.step(thrust_mag, stepT)
+
+            # episonde_eperience.experience_replay(np.reshape(np.array([s, a, r, s1, d]), [1, 5]))
 
             # Obtain the Q value
             Q1 = sess.run(Q_output, feed_dict={inputs:np.identity(num_inputs)[s1:s1+1]})
@@ -148,6 +174,8 @@ with tf.Session() as sess:
         j_list.append(j)
         r_list.append(rall)
 
+        # experience.experience_replay(episonde_eperience)
+
         # if i % 100 == 0:
             # env.render_plots()
             # env.render()
@@ -161,6 +189,10 @@ with tf.Session() as sess:
     print("Score: " + str(sum(r_list)))
     plt.plot(r_list)
     plt.show()
+
+
+
+
 
 
 
