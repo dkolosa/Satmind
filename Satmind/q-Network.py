60e92daca97b0f0c40a4e5cba4a2d4ecc5b210da
Daniel Kolosa <contact@dkolosa.com>
2018-09-19 00:35:42 -0400

    track states a and aDot Tweaks to NN and reward function added tracking for loss function and thrust actions

--- Satmind/q-Network.py
+++ Satmind/q-Network.py
@@ -20,6 +20,7 @@ class Experience:
 
 
 if __name__ == '__main__':
+
     # Create the enviornment
     # env = gym.make('FrozenLake-v0')
     # env.reset()
@@ -63,16 +64,16 @@ if __name__ == '__main__':
     env.create_orbit(state_targ, final_date, target=True)
 
     env._extrap_Date = env._initial_date
-    stepT = 100.0
+    stepT = 1000.0
 
     # thrust_mag = 0.0
 
-    thrust_mag = [0.0, 0.1, 0.25, 0.5, 0.75, 1.0]
-
+    thrust_values = [0.0, 0.25, 0.50, 1.0, 5.0, 10.0]
+    thrust_mag = [0, 1, 2, 3, 4, 5]
     # learning parameters
-    y = .99
+    y = .95
     e = 0.01
-    num_episodes = 100
+    num_episodes = 5
     # steps and rewards per episode (respectively)
     j_list = []
     r_list = []
@@ -86,12 +87,11 @@ if __name__ == '__main__':
     # TODO: one-hot encode output acitons
         # [[1,0,0,0,0,0],[0,1,0,0,0,0],...]
 
-    layer_1_nodes = 200
-    layer_2_nodes = 200
+    layer_1_nodes = 512
+    layer_2_nodes = 512
 
     # Establish feed-forward network
     inputs = tf.placeholder(shape=[1, num_inputs], dtype=tf.float32)
-
     # w1 = tf.Variable(tf.zeros[16,100])
     # b1 = tf.variable(tf.zeros[100])
 
@@ -105,24 +105,16 @@ if __name__ == '__main__':
         weights = tf.get_variable(name='weights-2', shape=(layer_1_nodes, layer_2_nodes))
         layer_2_output = tf.nn.tanh(tf.matmul(layer_1_output, weights))
 
-    # weights2 = tf.Variable(tf.random_uniform([16,4]
     with tf.variable_scope('output'):
         weights = tf.get_variable(name='weight-out', shape=(layer_2_nodes, num_outputs))
         # bias = tf.get_variable(name='bias_out', shape=([num_outputs]), initializer=tf.zeros_initializer())
-
-        # Q_output = tf.nn.relu(tf.matmul(layer_1_output, weights) + bias)
         Q_output = tf.matmul(layer_2_output, weights)
+
+    next_Q = tf.placeholder(shape=[1, num_outputs], dtype=tf.float32)
 
     predict = tf.argmax(Q_output, 1)
 
-    # actions = tf.placeholder(shape=[None], dtype=tf.int32)
-    # actions_onehot = tf.one_hot(actions, num_outputs, dtype=tf.float32)
-
-    # Q = tf.reduce_sum(tf.multiply(Q_output, actions_onehot), reduction_indices=1)
-
     # Sum of squares loss between target and predicted Q
-    next_Q = tf.placeholder(shape=[1, num_outputs], dtype=tf.float32)
-    # next_Q = tf.placeholder(shape=[None], dtype=tf.float32)
     loss = tf.reduce_sum(tf.square(next_Q - Q_output))
     trainer = tf.train.AdamOptimizer(learning_rate=0.001)
     update = trainer.minimize(loss)
@@ -143,78 +135,66 @@ if __name__ == '__main__':
     # Start tensorflow session
     with tf.Session() as sess:
         sess.run(init)
-        for i in range(num_episodes):
+        for i in range(1,num_episodes):
             # reset enviornment to get first observation
             s = env.reset()
             rall = 0
+            r = 0
             d = False
             j = 0
             # episonde_eperience = Experience(buffer_size=50)
-
             # Q-network
             # while j < 99:
+            actions = []
+            loss_tot = []
             while env._extrap_Date.compareTo(final_date) <= 0:
-                actions = []
+
                 j += 1
                 # choose an action
                 # This value is thrust
                 a, allQ = sess.run([predict, Q_output], feed_dict={inputs: [s]})
-                # print(Q_output)
 
-                # print('action: ', a)
-                # print('allQ: ', allQ)
-                # a, allQ = sess.run([predict, Q_output],
-                #                    feed_dict={inputs:np.identity(num_inputs)[s:s+1]})
                 if np.random.rand(1) < e:
-                    # a[0] = env.action_space.sample()
                     a[0] = random.randint(0, len(thrust_mag)-1)
                     # print("Random Hit!")
-                # print("a[0]: ", a[0])
+
                 # Get a new state and reward
                 # The state is the x-y coordinates, r =0 if not reached
-                # s1, r, d, _ = env.step(thrust_mag[a[0]], stepT)
-
                 if a == 0:
-                    action = thrust_mag[0]
+                    action = thrust_values[0]
                 elif a == 1:
-                    action = thrust_mag[1]
+                    action = thrust_values[1]
                 elif a == 2:
-                    action = thrust_mag[2]
+                    action = thrust_values[2]
                 elif a == 3:
-                    action = thrust_mag[3]
+                    action = thrust_values[3]
                 elif a == 4:
-                    action = thrust_mag[4]
+                    action = thrust_values[4]
                 elif a == 5:
-                    action = thrust_mag[5]
+                    action = thrust_values[5]
                 else:
                     action = thrust_mag[0]
 
                 s1, r, done, _ = env.step(action, stepT)
                 actions.append(action)
-                # env.shift_date(stepT)
-
-                # s1 = env.step(thrust_mag, stepT)
-
-                # episonde_eperience.experience_replay(np.reshape(np.array([s, a, r, s1, d]), [1, 5]))
 
                 # Obtain the Q value
                 Q1 = sess.run(Q_output, feed_dict={inputs: [s1]})
+
                 # Get maxQ and set target value for chosen action
                 maxQ1 = np.max(Q1)
                 targetQ = allQ
                 targetQ[0, a[0]] = r + y * maxQ1
 
                 # Train the NN using target and predicted Q values
-                # _, W1 = sess.run([update, weights],
-                # feed_dict={inputs:np.identity(num_inputs)[s:s+1],next_Q:targetQ})
-
                 _, W1 = sess.run([update, weights], feed_dict={inputs: [s], next_Q: targetQ})
                 rall += r
                 s = s1
-                actions.append(action)
 
-
-
+                opt = sess.run(loss, feed_dict={inputs: [s], next_Q: targetQ})
+                # print("==============")
+                # print("loss: ", opt)
+                loss_tot.append(opt)
                 if done:
                     # Random action
                     e = 1.0 / ((i / 50) + 10)
@@ -224,7 +204,7 @@ if __name__ == '__main__':
                     plt.xlabel('km')
                     plt.ylabel('km')
                     plt.subplot(2, 1, 2)
-                    plt.plot(r_list)
+                    plt.plot(action)
                     plt.show()
                     break
 
@@ -234,11 +214,12 @@ if __name__ == '__main__':
             print('a final {}'.format(env._currentOrbit.getA()/1e3))
 
             # experience.experience_replay(episonde_eperience)
+            # plt.plot(j_list, opt)
+            # plt.plot(actions)
 
+            plt.show()
             if i % 5 == 0:
-            # plt.plot(env._px, env._py)
-            # env.render_plots()
-            # env.render()
+
                 plt.title('iteration {}'.format(i))
                 plt.subplot(2, 1, 1)
                 plt.plot(np.asarray(env._px)/1e3, np.asarray(env._py)/1e3)
@@ -250,13 +231,7 @@ if __name__ == '__main__':
                 # plt.plot(actions)
                 plt.show()
             print("episode {} of {}".format(i, num_episodes))
+        plt.plot(loss_tot)
+        plt.show()
+        print('orbit:{}'.format(env._currentOrbit.getA()/1e3))
 
-        # print('KEY:\nSFFF(S: starting point, safe)\n',
-        #       'FHFH(F: frozen surface, safe)\n',
-        #       'FFFH(H: hole, fall to your doom)\n',
-        #       'HFFG(G: goal, where the frisbee is located)')
-        # print("successful episodes " + str(sum(r_list)/num_episodes*100) + "%")
-        # print("Score: " + str(sum(r_list)))
-        print('orbit:{}'.format(env._currentOrbit.getA()))
-        # plt.show()
-        # print(r_list)
