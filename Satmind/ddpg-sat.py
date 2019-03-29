2nd layer batch normalization for actor fixed file save simplified reward function

Satmind/ddpg-sat.py
@@ -60,13 +60,13 @@ class Actor:
                                       units=self.layer_2_nodes,
                                       activation=tf.nn.relu,
                                       )
+            l2_batch = tf.layers.batch_normalization(layer_2)
 
         with tf.variable_scope(str(name) + '_output'):
-            output = tf.layers.dense(inputs=layer_2,
+            output = tf.layers.dense(inputs=l2_batch,
                                           units=self.n_actions,
                                           activation=tf.nn.tanh,
                                           kernel_initializer=tf.random_uniform_initializer(-0.003,0.003),
-                                          use_bias=False,
                                      )
 
         scaled_output = tf.multiply(output, self.action_bound)
@@ -325,7 +325,7 @@ def orekit_setup():
         duration = mission['duration']
 
     stepT = 100.0
-    duration = (24.0 * 60.0 ** 2) * duration
+    duration = (24.0 * 60.0 ** 2) * 4
 
     env = OrekitEnv(state, state_targ, date, duration, mass, fuel_mass, stepT)
     return env, duration
@@ -333,30 +333,30 @@ def orekit_setup():
 
 def main(args):
     ENVS = ('Pendulum-v0', 'MountainCarContinuous-v0', 'BipedalWalker-v2', 'OrekitEnv-v0')
-    ENV = ENVS[3]
-    # env = gym.make(ENV)
-    env, duration = orekit_setup()
+    ENV = ENVS[0]
+    env = gym.make(ENV)
+    # env, duration = orekit_setup()
 
     # env.seed(1234)
     np.random.seed(1234)
 
     num_episodes = 800
     stepT = 100.0
-    iter_per_episode = int(duration / stepT)
+    # iter_per_episode = int(duration / stepT)
     batch_size = 1
-    # iter_per_episode = 200
+    iter_per_episode = 200
 
 
     # Network inputs and outputs
-    features = env.observation_space
-    n_actions = 3
-    action_bound = 0.7
+    # features = env.observation_space
+    # n_actions = 3
+    # action_bound = 3.0
 
-    # features = env.observation_space.shape[0]
-    # n_actions = env.action_space.shape[0]
-    # action_bound = env.action_space.high
+    features = env.observation_space.shape[0]
+    n_actions = env.action_space.shape[0]
+    action_bound = env.action_space.high
 
-    layer_1_nodes, layer_2_nodes = 600, 500
+    layer_1_nodes, layer_2_nodes = 800, 750
     tau = 0.001
     actor_lr, critic_lr = 0.0001, 0.001
     GAMMA = 0.99
@@ -379,7 +379,7 @@ def main(args):
         TRAIN = True
         today = datetime.date.today()
         path = '/tmp/ddpg_models/'
-        checkpoint_path =path+str(today)+'-'+ENV
+        checkpoint_path =path+str(today)+'-'+ENV+'/'
         os.makedirs(checkpoint_path, exist_ok=True)
         print(f'Model will be saved in: {checkpoint_path}')
 
@@ -400,13 +400,13 @@ def main(args):
                 actions = []
                 for j in range(iter_per_episode):
 
-                    # env.render()
+                    env.render()
 
                     # Select an action
-                    a = actor.predict(np.reshape(s, (1, features)), sess) + actor_noise()
+                    a = actor.predict(np.reshape(s, (1, features)), sess)
 
                     # Observe state and reward
-                    s1, r, done = env.step(a[0])
+                    s1, r, done, _ = env.step(a[0])
 
                     actions.append(a)
                     # Store in replay memory
@@ -449,12 +449,15 @@ def main(args):
                     # if done:
                     if done or j >= iter_per_episode - 1:
                         print('Episode: {}, reward: {}, Q_max: {}'.format(i, int(sum_reward), sum_q/float(j)))
-                        print(f'diff:   a: {(env.r_target_state[0] - env._currentOrbit.getA())/1e3},\n'
-                              f'ex: {env.r_target_state[1] - env._currentOrbit.getEquinoctialEx()},\t'
-                              f'ey: {env.r_target_state[2] - env._currentOrbit.getEquinoctialEy()},\n'
-                              f'hx: {env.r_target_state[3] - env._currentOrbit.getHx()},\t'
-                              f'hy: {env.r_target_state[4] - env._currentOrbit.getHy()}')
+                        # print(f'diff:   a: {(env.r_target_state[0] - env._currentOrbit.getA())/1e3},\n'
+                        #       f'ex: {env.r_target_state[1] - env._currentOrbit.getEquinoctialEx()},\t'
+                        #       f'ey: {env.r_target_state[2] - env._currentOrbit.getEquinoctialEy()},\n'
+                        #       f'hx: {env.r_target_state[3] - env._currentOrbit.getHx()},\t'
+                        #       f'hy: {env.r_target_state[4] - env._currentOrbit.getHy()}')
                         print('===========')
+                        # thrust_mag = np.linalg.norm(np.asarray(actions), axis=1)
+                        # plt.plot(np.arange(len(thrust_mag)),thrust_mag)
+                        # plt.show()
                         break
                 if i % 50 == 0:
                     # plt.plot(np.linalg.norm(np.asarray(actions), axis=1))
@@ -462,11 +465,13 @@ def main(args):
                     # plt.ylabel('Thrust (N)')
                     # plt.tight_layout()
                     # plt.show()
-                    env.render_plots()
+                    saver.save(sess, checkpoint_path)
+                    print(f'Model Saved and Updated')
+                    # env.render_plots()
             # Save the trained model
                 if i % 50 == 0:
                     if args['model_dir'] is not None:
-                        saver.save(sess, checkpoint_path, write_meta_graph=False)
+                        saver.save(sess, checkpoint_path)
                         print(f'Model Saved and Updated')
         else:
             if args['model_dir'] is not None:

Satmind/env_orekit.py
@@ -348,11 +348,9 @@ class OrekitEnv:
 
             if thrust_mag <= 0:
                 # DIRECTION = Vector3D.MINUS_J
-                # DIRECTION = Vector3D(float(thrust_dir[0]), float(thrust_dir[1]), float(thrust_dir[2]))
                 thrust_mag = abs(float(thrust_mag))
             else:
                 # DIRECTION = Vector3D.PLUS_J
-                # DIRECTION = Vector3D(float(thrust_dir[0]), float(thrust_dir[1]), float(thrust_dir[2]))
                 thrust_mag = float(thrust_mag)
 
         thrust_force = ConstantThrustManeuver(self._extrap_Date, self.stepT, thrust_mag, self._isp, attitude, DIRECTION)
@@ -408,11 +406,13 @@ class OrekitEnv:
             reward = -100
 
         reward = -abs(self.r_target_state[0] - state[0]) / self._orbit.getA() - \
-                 np.nan_to_num(abs(self.r_target_state[1] - state[1])) - \
-                 np.nan_to_num(abs(self.r_target_state[2] - state[2])) - \
-                 np.nan_to_num(abs(self.r_target_state[3] - state[3]) / self._orbit.getHx()) - \
-                 np.nan_to_num(abs(self.r_target_state[4] - state[4]) / self._orbit.getHy()) - \
-                 0.01 * abs(thrust)
+                 np.nan_to_num(abs(self._targetOrbit.getE() - self._currentOrbit.getE())) - \
+                 np.nan_to_num(abs(self._targetOrbit.getI() - self._currentOrbit.getI())) - \
+                 0.001 * thrust
+
+        if abs(self.r_target_state[0] - state[0]) <= self._orbit_tolerance['a']:
+            print(f'sma hit!!')
+            reward = 10
 
         if abs(self.r_target_state[0] - state[0]) <= self._orbit_tolerance['a'] and \
            abs(self.r_target_state[1] - state[1]) <= self._orbit_tolerance['ex'] and \
@@ -424,9 +424,9 @@ class OrekitEnv:
             done = True
             print('hit')
 
-        if self.r_target_state[0] - state[0] <= -10000:
-            reward = -100
-            done = True
+        # if self.r_target_state[0] - state[0] <= -10000:
+        #     reward = -100
+        #     done = True
 
         return reward, done
 