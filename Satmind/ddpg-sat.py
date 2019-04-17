16cee555958c0181d8f7c233887174629f74c1ff
Daniel Kolosa <contact@dkolosa.com>
2019-04-17 11:25:21 -0400

    Moved noise to a utility module

--- Satmind/ddpg-sat.py
+++ Satmind/ddpg-sat.py
@@ -11,28 +11,7 @@ import json
 
 import Satmind.actor_critic as models
 from Satmind.env_orekit import OrekitEnv
-
-class OrnsteinUhlenbeck():
-    def __init__(self, mu, sigma=0.3, theta=.15, dt=1e-2, x0=None):
-        self.theta = theta
-        self.mu = mu
-        self.sigma = sigma
-        self.dt = dt
-        self.x0 = x0
-        self.reset()
-
-    def __call__(self):
-        x = self.x_prev + self.theta * (self.mu - self.x_prev) * self.dt + \
-                self.sigma * np.sqrt(self.dt) * np.random.normal(size=self.mu.shape)
-        self.x_prev = x
-        return x
-
-    def reset(self):
-        self.x_prev = self.x0 if self.x0 is not None else np.zeros_like(self.mu)
-
-    def __repr__(self):
-        return 'OrnsteinUhlenbeckActionNoise(mu={}, sigma={})'.format(self.mu, self.sigma)
-
+import Satmind.utils
 
 class Experience:
     def __init__(self, buffer_size):
@@ -144,7 +123,7 @@ def main(args):
 
     # Initialize actor and critic network and targets
     actor = models.Actor(features, n_actions, layer_1_nodes, layer_2_nodes, action_bound, tau, actor_lr, batch_size, 'actor')
-    actor_noise = OrnsteinUhlenbeck(np.zeros(n_actions))
+    actor_noise = Satmind.utils.OrnsteinUhlenbeck(np.zeros(n_actions))
     critic = models.Critic(features, n_actions, layer_1_nodes, layer_2_nodes, critic_lr, tau, 'critic', actor.trainable_variables)
 
     # Replay memory buffer
