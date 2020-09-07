cf3d9cebcadf197585dc20009650622a69759165
Daniel <contact@dkolosa.com>
2020-09-07 09:17:05 -0400

    implementing model learning

--- Satmind/tf2model.py
+++ Satmind/tf2model.py
@@ -5,19 +5,17 @@ import os
 
 
 class Critic(tf.keras.Model):
-
     def __init__(self, layer_1, layer_2, name='Critic',
                  checkpoint_dir=''):
         super(Critic, self).__init__()
         self.layer_1 = Dense(layer_1, activation='relu')
         self.layer_2 = Dense(layer_2, activation='relu')
         self.q = Dense(1, activation=None)
-
-        self.name= name
+        self.name = name
         self.checkpoint_dir = checkpoint_dir
         self.checkpoint = os.path.join(self.checkpoint_dir, name+ '_ddpg.h5')
 
-    def forward(self, state, action):
+    def call(self, state, action):
         action = self.layer_1(tf.concat([state, action], axis=1))
         action = self.layers_2(action)
         q = self.q(action)
@@ -25,17 +23,17 @@ class Critic(tf.keras.Model):
 
 
 class Actor(tf.keras.Model):
-    def __init__(self, n_actions, layer_1=64, layer_2=32, name="Actor",
+    def __init__(self, n_actions, layer_1=64, layer_2=32, name='Actor',
                  checkpoint_dir=''):
         super(Actor, self).__init__()
         self.layer_1 = Dense(layer_1, activation='relu')
         self.layer_2 = Dense(layer_2, activation='relu')
         self.action = Dense(n_actions, activation='tanh')
-        self.name= name
+        self.name = name
         self.checkpoint_dir = checkpoint_dir
         self.checkpoint = os.path.join(self.checkpoint_dir, name+ '_ddpg.h5')
 
-    def forward(self,x):
+    def call(self, x):
         x = self.layer_1(x)
         x = tf.keras.layers.BatchNormalization(x)
         x = self.layer_2(x)
---
+++ ddpg-tf2.py
@@ -0,0 +1,147 @@
+import tensorflow as tf
+from tensorflow.losses import mean_squared_error
+from tensorflow.keras.optimizers import Adam
+import numpy as np
+import gym
+import gym.spaces
+import matplotlib.pyplot as plt
+from Satmind.tf2model import Critic, Actor
+from Satmind.utils import OrnsteinUhlenbeck, AdaptiveParamNoiseSpec
+from Satmind.replay_memory import Per_Memory, Uniform_Memory
+
+
+def test_rl():
+    ENVS = ('Pendulum-v0', 'MountainCarContinuous-v0', 'BipedalWalker-v2', 'LunarLanderContinuous-v2',
+            'BipedalWalkerHardcore-v2')
+
+    ENV = ENVS[0]
+    env = gym.make(ENV)
+    iter_per_episode = 200
+    features = env.observation_space.shape[0]
+    n_actions = env.action_space.shape[0]
+    action_bound = env.action_space.high
+
+    env.seed(1234)
+    np.random.seed(1234)
+
+    num_episodes = 1001
+
+    batch_size = 64
+    #Pendulum
+    layer_1_nodes, layer_2_nodes = 120, 64
+    #lander
+
+    tau = 0.001
+    actor_lr, critic_lr = 0.0001, 0.001
+    GAMMA = 0.99
+    ep = 0.001
+
+    actor = Actor(n_actions)
+    critic = Critic()
+
+    actor_target = Actor(n_actions)
+    critic_target = Critic()
+
+    actor_noise = OrnsteinUhlenbeck()
+
+    actor.compile(optimizer=Adam(learning_rate=actor_lr))
+    critic.compile(optimizer=Adam(learning_rate=critic_lr))
+
+    PER = True
+    if PER:
+        memory = Per_Memory(capacity=100000)
+    else:
+        memory = Uniform_Memory(buffer_size=1000)
+
+    for i in range(num_episodes):
+        s = env.reset()
+        sum_reward = 0
+        sum_q = 0
+        j = 0
+
+        while True:
+            env.render()
+
+            a = actor.predict(s + actor_noise())
+            s1, r, done, _ = env.step(a[0])
+
+            # Store in replay memory
+            if PER:
+                error = abs(r + ep)  # D_i = max D
+                memory.add(error, (
+                np.reshape(s, (features,)), np.reshape(a[0], (n_actions,)), r, np.reshape(s1, (features,)), done))
+            else:
+                memory.add(
+                    (np.reshape(s, (features,)), np.reshape(a[0], (n_actions,)), r, np.reshape(s1, (features,)), done))
+
+            # sample from memory
+            # if batch_size < memory.get_count:
+            # mem = memory.sample(batch_size)
+            # s_rep = np.array([_[0] for _ in mem])
+            # a_rep = np.array([_[1] for _ in mem])
+            # r_rep = np.array([_[2] for _ in mem])
+            # s1_rep = np.array([_[3] for _ in mem])
+            # d_rep = np.array([_[4] for _ in mem])
+
+            if batch_size < memory.count:
+                mem, idxs, isweight = memory.sample(batch_size)
+                s_rep = np.array([_[0] for _ in mem])
+                a_rep = np.array([_[1] for _ in mem])
+                r_rep = np.array([_[2] for _ in mem])
+                s1_rep = np.array([_[3] for _ in mem])
+                d_rep = np.array([_[4] for _ in mem])
+
+                # Get q-value from the critic target
+                act_target = actor.predict(s1_rep)
+
+                target_q = critic.predict(s1_rep, act_target)
+
+                y_i = []
+                for x in range(batch_size):
+                    if d_rep[x]:
+                        y_i.append(r_rep[x])
+                    else:
+                        y_i.append(r_rep[x] + GAMMA * target_q[x])
+
+                # update the critic network
+                error, predicted_q, _ = critic.fit(s_rep, a_rep, np.reshape(y_i, (batch_size, 1)),
+                                                     np.reshape(isweight, (batch_size, 1)), sess)
+                loss = mean_squared_error(y_i, critic.predict(s_rep, a_rep))
+                opt = Adam(loss)
+                for n in range(batch_size):
+                    idx = idxs[n]
+                    memory.update(idx, abs(error[n][0]))
+
+                sum_q += np.amax(predicted_q)
+
+
+                # update actor policy
+                # a_output = actor.predict(s_rep, sess)
+                # grad = critic.action_gradient(s_rep, a_output, sess)
+                # actor.train(s_rep, grad[0], sess)
+
+                # update target networks
+                # actor.update_target_network(sess)
+                # critic.update_target_network(sess)
+
+            sum_reward += r
+
+            s = s1
+            j += 1
+            if done:
+                print('Episode: {}, reward: {}, Q_max: {}'.format(i, int(sum_reward), sum_q / float(j)))
+                # rewards.append(sum_reward)
+                print('===========')
+                saver.save(sess, 'test_walkder/model.ckpt')
+                break
+
+
+
+def loss_fn(y_true, y_pred, importance):
+    error = tf.math.square(y_true, y_pred)
+    return tf.reduce_mean(tf.math.multiply(error, importance))
+
+
+
+if __name__ == '__main__':
+    test_rl()
